a203e30506407988acfd604cc4c37ddab1f8abdf
Pierre Chifflier <chifflier@inl.fr>
2003-02-14 14:02:04 +0000

    Modified clientThreadProc prototype to match thread definition.

--- src/wzd_ClientThread.h
+++ src/wzd_ClientThread.h
@@ -4,7 +4,7 @@
 int clear_read(int sock, char *msg, unsigned int length, int flags, int timeout, void * vcontext);
 int clear_write(int sock, const char *msg, unsigned int length, int flags, int timeout, void * vcontext);
 
-void clientThreadProc(void *arg);
+void * clientThreadProc(void *arg);
 
 void client_die(wzd_context_t * context);
 
