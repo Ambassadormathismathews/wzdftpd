475510545fe7123534bd40c8a784907dde0a4d85
Pierre Chifflier <chifflier@inl.fr>
2003-02-20 10:22:18 +0000

    Compilation ok with cygwin Added speed.

--- tools/sitewho/sitewho.c
+++ tools/sitewho/sitewho.c
@@ -1,11 +1,19 @@
+#ifdef __CYGWIN__
+#include <w32api/windows.h>
+#else /* __CYGWIN__ */
 # include <sys/types.h>
 # include <sys/ipc.h>
 # include <sys/shm.h>
+#endif /* __CYGWIN__ */
 
 #include <stdio.h>
 #include <wzd.h>
+/*#include <wzd_shm.h>*/
 
 #define	SHM_KEY	0x1331c0d3
+
+/* FIXME ... this is an unresolved symbol */
+unsigned int wzd_server_uid;
 
 int main(int argc, char *argv[])
 {
@@ -16,13 +24,27 @@ int main(int argc, char *argv[])
   wzd_user_t * user_list;
   wzd_group_t * group_list;
   int i,found=0;
+/*  wzd_shm_t * shm;*/
+#ifdef __CYGWIN__
+  void * handle;
+  char name[256];
+#endif
 
   length += HARD_USERLIMIT*sizeof(wzd_context_t);
   length += HARD_DEF_USER_MAX*sizeof(wzd_user_t);
   length += HARD_DEF_GROUP_MAX*sizeof(wzd_group_t);
 
+#ifdef __CYGWIN__
+  sprintf(name,"%lu",SHM_KEY);
+  handle = OpenFileMapping(FILE_MAP_ALL_ACCESS,FALSE,name);
+  if (handle == NULL)
+#else
   shmid = shmget(SHM_KEY,0,0400);
-  if (shmid == -1) {
+/*  shm = wzd_shm_get(SHM_KEY,0400);*/
+  if (shmid == -1)
+/*  if (!shm) */
+#endif
+  {
     fprintf(stderr,"shmget failed\n");
     fprintf(stderr,"This is probably due to\n");
     fprintf(stderr,"\t* server not started\n");
@@ -30,11 +52,18 @@ int main(int argc, char *argv[])
     return -1;
   }
 
+#ifdef __CYGWIN__
+  datazone = MapViewOfFile(handle,FILE_MAP_ALL_ACCESS,0, 0, 0);
+  if (datazone == NULL)
+#else
   datazone = shmat(shmid,NULL,SHM_RDONLY);
-  if (datazone == (void*)-1) {
+  if (datazone == (void*)-1)
+#endif
+  {
     fprintf(stderr,"shmat failed\n");
     return -1;
   }
+/*  datazone = shm->datazone;*/
 
   context_list = (wzd_context_t*)datazone;
   i = HARD_USERLIMIT;
@@ -68,16 +97,34 @@ int main(int argc, char *argv[])
 	snprintf(hostip,18,"%d.%d.%d.%d",
 	    context->hostip[0],context->hostip[1],context->hostip[2],
 	    context->hostip[3]);
+	/* XXX hide ip */
+	strcpy(hostip,"xxx.xxx.xxx.xxx");
         fprintf(stdout,"|%15s|   %15s|%16s|%20s |\n",
             user->username?user->username:"NULL",
             user->username?user->tagline:"NULL",
 	    hostip,
 	    context->last_command);
+	if (strncasecmp(context->last_command,"retr",4)==0) {
+	  fprintf(stdout,"|  %.1f kB/s  |\n",context->current_dl_limiter.current_speed/1024.f);
+	} else {
+	  if (strncasecmp(context->last_command,"stor",4)==0) {
+	    fprintf(stdout,"|  %.1f kB/s  |\n",context->current_ul_limiter.current_speed/1024.f);
+	  }
+	}
       }
     }
     fprintf(stdout,"|---------------.------------------.----------------.---------------------|\n");
   }
 
+#ifdef __CYGWIN__
+  CloseHandle(handle);
+#else
   shmdt(datazone);
+  /* FIXME wzd_shm_free does NOT work as it tries to destroy a semaphore
+   * resulting in a SIGSEGV ...
+   */
+/*  wzd_shm_free(shm);*/
+/*  shmdt(shm->datazone);*/
+#endif
   return 0;
 }
