be03f127dedce6d4a638b4b49eae98bd8aaefeb7
Pierre Chifflier <chifflier@inl.fr>
2003-03-07 12:28:49 +0000

    Allowed use of shm even in multithread mode.

--- src/wzd_shm.c
+++ src/wzd_shm.c
@@ -21,6 +21,11 @@
 
 #ifdef WZD_MULTITHREAD
 
+#ifndef __CYGWIN__
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#endif /* __CYGWIN__ */
+
 /* creates a semaphore */
 wzd_sem_t wzd_sem_create(unsigned long key, int nsems, int flags)
 {
@@ -49,89 +54,11 @@ int wzd_sem_unlock(wzd_sem_t sem, int n)
   return sem_post((sem_t*)sem);
 }
 
-/* creates an shm zone */
-wzd_shm_t * wzd_shm_create(unsigned long key, int size, int flags)
-{
-  wzd_shm_t *shm;
-
-  shm = malloc(sizeof(wzd_shm_t));
-  if (!shm) return NULL;
-
-  shm->datazone = malloc(size);
-#ifdef __CYGWIN__
-  shm->handle=NULL;
-#else
-  shm->shmid=0;
-#endif
-  shm->semid = wzd_sem_create(key,1,0);
-
-  return shm;
-}
-
-/* returns an EXISTING shm zone */
-wzd_shm_t * wzd_shm_get(unsigned long key, int flags)
-{
-  return NULL;
-}
-
-/* read mem */
-int wzd_shm_read(wzd_shm_t * shm, void * data, int size, int offset)
-{
-  /* locks zone */
-  if (sem_wait((sem_t*)shm->semid)<0) {
-fprintf(stderr,"CRITICAL: could not lock sem - errno is %d (%s)\n",errno,strerror(errno));
-    return -1;
-  }
-  /* read data */
-  memcpy(data,shm->datazone+offset,size);
-  /* unlocks zone */
-  if (sem_post((sem_t*)shm->semid)<0) {
-fprintf(stderr,"CRITICAL: could not unlock sem - errno is %d (%s)\n",errno,strerror(errno));
-    return -1;
-  }
-
-  return 0;
-}
-
-/* writes mem */
-int wzd_shm_write(wzd_shm_t * shm, void * data, int size, int offset)
-{
-  /* locks zone */
-  if (sem_wait((sem_t*)shm->semid)<0) {
-fprintf(stderr,"CRITICAL: could not lock sem - errno is %d (%s)\n",errno,strerror(errno));
-    return -1;
-  }
-  /* write data */
-  memcpy(shm->datazone+offset,data,size);
-  /* unlocks zone */
-  if (sem_post((sem_t*)shm->semid)<0) {
-fprintf(stderr,"CRITICAL: could not unlock sem - errno is %d (%s)\n",errno,strerror(errno));
-    return -1;
-  }
-
-  return 0;
-}
-
-/* destroys shm */
-void wzd_shm_free(wzd_shm_t * shm)
-{
-  if (!shm) return;
-
-  sem_destroy((sem_t*)shm->semid);
-  free(shm->datazone);
-  shm->datazone = 0;
-}
-
-/* cleanup if previous exec has crashed */
-void wzd_shm_cleanup(unsigned long key)
-{
-}
-
-
-#else /* WZD_MULTITHREAD */
+#endif /* WZD_MULTITHREAD */
 
 #ifdef __CYGWIN__
 
+#ifndef WZD_MULTITHREAD
 /* create a semaphore */
 wzd_sem_t wzd_sem_create(unsigned long key, int nsems, int flags)
 {
@@ -160,7 +87,7 @@ int wzd_sem_unlock(wzd_sem_t sem, int n)
   LeaveCriticalSection(sem);
   return 0;
 }
-
+#endif /* WZD_MULTITHREAD */
 
 /* creates an shm zone */
 wzd_shm_t * wzd_shm_create(unsigned long key, int size, int flags)
@@ -245,8 +172,8 @@ void wzd_shm_cleanup(unsigned long key)
 
 #else /* __CYGWIN__ */
 
-#include <sys/ipc.h>
-#include <sys/shm.h>
+#ifndef WZD_MULTITHREAD
+
 #include <sys/sem.h>
 
 typedef union semun {
@@ -300,13 +227,14 @@ int wzd_sem_unlock(wzd_sem_t sem, int n)
   buf.sem_flg = SEM_UNDO;
   return semop( sem, &buf, 1 );
 }
+#endif /* WZD_MULTITHREAD */
 
 /* creates an shm zone */
 wzd_shm_t * wzd_shm_create(unsigned long key, int size, int flags)
 {
   wzd_shm_t *shm;
-  semun_t u_semun;
-  unsigned short array[1];
+/*  semun_t u_semun;
+  unsigned short array[1];*/
   int have_set_uid=0;
 
   shm = malloc(sizeof(wzd_shm_t));
@@ -342,20 +270,13 @@ fprintf(stderr,"CRITICAL: could not shmat, key %lu, size %d - errno is %d (%s)\n
     shmctl(shm->shmid,IPC_RMID,NULL);
     return NULL;
   }
+  shm->semid = wzd_sem_create(key,1,0);
 
-  shm->semid = semget((key_t)key,1,0);
-
-  if ( ! (shm->semid==-1 && errno==ENOENT) ) {
-fprintf(stderr,"CRITICAL: sem exists with selected sem_key 0x%lx - check your config file\n",key);
-    if (have_set_uid) setreuid(-1,0); /* become root again */
-    shmdt(shm->datazone);
-    shmctl(shm->shmid,IPC_RMID,NULL);
-    return NULL;
-  }
-
-  shm->semid = semget((key_t)key,1,IPC_CREAT|IPC_EXCL|0600);
-
+#if WZD_MULTITHREAD
+  if (!shm->semid) {
+#else
   if (shm->semid == -1) {
+#endif
 fprintf(stderr,"CRITICAL: could not semget, key %lu - errno is %d (%s)\n",key,errno,strerror(errno));
     if (have_set_uid) setreuid(-1,0); /* become root again */
     shmdt(shm->datazone);
@@ -363,16 +284,6 @@ fprintf(stderr,"CRITICAL: could not semget, key %lu - errno is %d (%s)\n",key,er
     return NULL;
   }
 
-  /* set sem value to 0 */
-  array[0] = 0;
-  u_semun.array = array;
-  if (semctl(shm->semid,0,SETALL,u_semun)<0) {
-fprintf(stderr,"CRITICAL: could not set sem value, key %lu - errno is %d (%s)\n",key,errno,strerror(errno));
-    if (have_set_uid) setreuid(-1,0); /* become root again */
-    shmdt(shm->datazone);
-    shmctl(shm->shmid,IPC_RMID,NULL);
-    return NULL;
-  }
 
   if (have_set_uid) setreuid(-1,0); /* become root again */
 
@@ -382,22 +293,81 @@ fprintf(stderr,"CRITICAL: could not set sem value, key %lu - errno is %d (%s)\n"
 /* returns an EXISTING shm zone */
 wzd_shm_t * wzd_shm_get(unsigned long key, int flags)
 {
-  return NULL;
+  wzd_shm_t *shm;
+  int have_set_uid=0;
+
+  shm = malloc(sizeof(wzd_shm_t));
+  if (!shm) return NULL;
+
+  /* usefull in simple get ? */
+#if 0
+  if (geteuid()==0) {
+    setreuid(-1,wzd_server_uid);
+    have_set_uid = 1;
+  }
+#endif
+
+  shm->datazone = NULL;
+  shm->shmid = shmget((key_t)key, 0, flags );
+
+  if (shm->shmid == -1) {
+    out_err(LEVEL_CRITICAL,"CRITICAL: could not shmget, key %lu, - errno is %d (%s)\n",
+    key,errno,strerror(errno));
+#if 0
+    if (have_set_uid) setreuid(-1,0); /* become root again */
+#endif
+    return NULL;
+  }
+
+  shm->datazone = shmat(shm->shmid,NULL,0);
+  if (shm->datazone == (void*)-1) {
+fprintf(stderr,"CRITICAL: could not shmat, key %lu - errno is %d (%s)\n",
+    key,errno,strerror(errno));
+#if 0
+    if (have_set_uid) setreuid(-1,0); /* become root again */
+#endif
+    shmctl(shm->shmid,IPC_RMID,NULL);
+    return NULL;
+  }
+
+  shm->semid = semget(key,1,0);
+#if WZD_MULTITHREAD
+  if (!shm->semid) {
+#else
+  if (shm->semid == -1) {
+#endif
+fprintf(stderr,"CRITICAL: could not semget, key %lu - errno is %d (%s)\n",key,errno,strerror(errno));
+#if 0
+    if (have_set_uid) setreuid(-1,0); /* become root again */
+#endif
+    shmdt(shm->datazone);
+    shmctl(shm->shmid,IPC_RMID,NULL);
+    return NULL;
+  }
+
+#if 0
+  if (have_set_uid) setreuid(-1,0); /* become root again */
+#endif
+
+  return shm;
 }
 
 /* read mem */
 int wzd_shm_read(wzd_shm_t * shm, void * data, int size, int offset)
 {
-  struct sembuf s;
+/*  struct sembuf s;*/
 
   if (!shm) return -1;
 
+#if 0
   /* get sem : P() */
   s.sem_num = 0;
   s.sem_op = -1;
   s.sem_flg = SEM_UNDO;
   if (semop(shm->semid,&s,1)<0) {
-fprintf(stderr,"CRITICAL: could not set sem value, sem %d - errno is %d (%s)\n",shm->semid,errno,strerror(errno));
+#endif
+  if (wzd_sem_lock(shm->semid,1)) {
+fprintf(stderr,"CRITICAL: could not set sem value,  %ld - errno is %d (%s)\n",(unsigned long)shm->semid,errno,strerror(errno));
 /*    shmdt(shm->datazone);
     shmctl(shm->shmid,IPC_RMID,NULL);*/
     return 1;
@@ -406,12 +376,15 @@ fprintf(stderr,"CRITICAL: could not set sem value, sem %d - errno is %d (%s)\n",
   /* read data */
   memcpy(data,shm->datazone+offset,size);
 
+#if 0
   /* restore sem : V() */
   s.sem_num = 0;
   s.sem_op = 1;
   s.sem_flg = SEM_UNDO;
   if (semop(shm->semid,&s,1)<0) {
-fprintf(stderr,"CRITICAL: could not restore sem value, sem %d - errno is %d (%s)\n",shm->semid,errno,strerror(errno));
+#endif
+  if (wzd_sem_unlock(shm->semid,1)) {
+fprintf(stderr,"CRITICAL: could not restore sem value, sem %ld - errno is %d (%s)\n",(unsigned long)shm->semid,errno,strerror(errno));
 /*    shmdt(shm->datazone);
     shmctl(shm->shmid,IPC_RMID,NULL);*/
     return 1;
@@ -423,16 +396,19 @@ fprintf(stderr,"CRITICAL: could not restore sem value, sem %d - errno is %d (%s)
 /* writes mem */
 int wzd_shm_write(wzd_shm_t * shm, void * data, int size, int offset)
 {
-  struct sembuf s;
+/*  struct sembuf s;*/
 
   if (!shm) return -1;
 
+#if 0
   /* get sem : P() */
   s.sem_num = 0;
   s.sem_op = -1;
   s.sem_flg = SEM_UNDO;
   if (semop(shm->semid,&s,1)<0) {
-fprintf(stderr,"CRITICAL: could not set sem value, sem %d - errno is %d (%s)\n",shm->semid,errno,strerror(errno));
+#endif
+  if (wzd_sem_lock(shm->semid,1)) {
+fprintf(stderr,"CRITICAL: could not set sem value, sem %ld - errno is %d (%s)\n",(unsigned long)shm->semid,errno,strerror(errno));
 /*    shmdt(shm->datazone);
     shmctl(shm->shmid,IPC_RMID,NULL);*/
     return 1;
@@ -441,12 +417,15 @@ fprintf(stderr,"CRITICAL: could not set sem value, sem %d - errno is %d (%s)\n",
   /* write data */
   memcpy(shm->datazone+offset,data,size);
 
+#if 0
   /* restore sem : V() */
   s.sem_num = 0;
   s.sem_op = 1;
   s.sem_flg = SEM_UNDO;
   if (semop(shm->semid,&s,1)<0) {
-fprintf(stderr,"CRITICAL: could not restore sem value, sem %d - errno is %d (%s)\n",shm->semid,errno,strerror(errno));
+#endif
+  if (wzd_sem_unlock(shm->semid,1)) {
+fprintf(stderr,"CRITICAL: could not restore sem value, sem %ld - errno is %d (%s)\n",(unsigned long)shm->semid,errno,strerror(errno));
 /*    shmdt(shm->datazone);
     shmctl(shm->shmid,IPC_RMID,NULL);*/
     return 1;
@@ -460,7 +439,8 @@ void wzd_shm_free(wzd_shm_t * shm)
 {
   if (!shm) return;
 
-  semctl(shm->semid,IPC_RMID,0);
+/*  semctl(shm->semid,IPC_RMID,0);*/
+  wzd_sem_destroy(shm->semid);
   shmdt(shm->datazone);
   shmctl(shm->shmid,IPC_RMID,NULL);
 }
@@ -468,18 +448,20 @@ void wzd_shm_free(wzd_shm_t * shm)
 /* cleanup if previous exec has crashed */
 void wzd_shm_cleanup(unsigned long key)
 {
+#ifndef WZD_MULTITHREAD
   unsigned int semid;
+#endif
 
   {
     unsigned int shmid;
     shmid = shmget((key_t)key,0,0600 );
     if (shmid != -1) shmctl(shmid,IPC_RMID,NULL);
   }
+#ifndef WZD_MULTITHREAD
   semid = semget((key_t)key,0,0 );
   if (semid != -1) semctl(semid,IPC_RMID,0);
+#endif
 }
-
-#endif /* WZD_MULTITHREAD */
 
 
 #endif /* __CYGWIN__ */
