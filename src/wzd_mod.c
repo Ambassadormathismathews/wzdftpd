50745cc2d865b0a5ec439dcfe206625836edf3f2
Pierre Chifflier <chifflier@inl.fr>
2003-03-31 16:15:53 +0000

    Added license file Added several functions to handle external hooks, to free memory at exit

--- src/wzd_mod.c
+++ src/wzd_mod.c
@@ -1,3 +1,27 @@
+/*
+ * wzdftpd - a modular and cool ftp server
+ * Copyright (C) 2002-2003  Pierre Chifflier
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * As a special exemption, Pierre Chifflier
+ * and other respective copyright holders give permission to link this program
+ * with OpenSSL, and distribute the resulting executable, without including
+ * the source code for OpenSSL in the source distribution.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <malloc.h>
@@ -18,9 +42,27 @@
 #include "wzd_mod.h"
 #include "wzd_log.h"
 #include "wzd_misc.h"
+#include "wzd_messages.h"
 
-/* free hook list */
-int hook_free(wzd_hook_t **hook_list)
+struct event_entry_t {
+  unsigned long mask;
+  const char *name;
+};
+
+struct event_entry_t event_tab[] = {
+  { EVENT_LOGIN, "LOGIN" },
+  { EVENT_LOGOUT, "LOGOUT" },
+  { EVENT_PREUPLOAD, "PREUPLOAD" },
+  { EVENT_POSTUPLOAD, "POSTUPLOAD" },
+  { EVENT_POSTDOWNLOAD, "POSTDOWNLOAD" },
+  { EVENT_MKDIR, "MKDIR" },
+  { EVENT_RMDIR, "RMDIR" },
+  { EVENT_SITE, "SITE" },
+  { 0, NULL },
+};
+
+/** free hook list */
+void hook_free(wzd_hook_t **hook_list)
 {
   wzd_hook_t * current_hook, * next_hook;
 
@@ -29,9 +71,14 @@ int hook_free(wzd_hook_t **hook_list)
   while (current_hook) {
     next_hook = current_hook->next_hook;
 
+    if (current_hook->external_command)
+      free(current_hook->external_command);
+    if (current_hook->opt) free(current_hook->opt);
 #ifdef DEBUG
     current_hook->mask = 0;
     current_hook->hook = NULL;
+    current_hook->external_command = NULL;
+    current_hook->opt=NULL;
     current_hook->next_hook = NULL;
 #endif /* DEBUG */
     free(current_hook);
@@ -40,10 +87,9 @@ int hook_free(wzd_hook_t **hook_list)
   }
 
   *hook_list = NULL;
-  return 0;
 }
 
-/* register a new hook */
+/** register a new hook */
 int hook_add(wzd_hook_t ** hook_list, unsigned long mask, void_fct hook)
 {
   wzd_hook_t * current_hook, * new_hook;
@@ -53,6 +99,7 @@ int hook_add(wzd_hook_t ** hook_list, unsigned long mask, void_fct hook)
 
   new_hook->mask = mask;
   new_hook->hook = hook;
+  new_hook->opt = NULL;
   new_hook->external_command = NULL;
   new_hook->next_hook = NULL;
 
@@ -81,6 +128,7 @@ int hook_add_external(wzd_hook_t ** hook_list, unsigned long mask, const char *c
 
   new_hook->mask = mask;
   new_hook->hook = NULL;
+  new_hook->opt = NULL;
   new_hook->external_command = strdup(command);
   new_hook->next_hook = NULL;
 
@@ -100,7 +148,101 @@ int hook_add_external(wzd_hook_t ** hook_list, unsigned long mask, const char *c
   return 0;
 }
 
-/* check a module file */
+int hook_add_custom_command(wzd_hook_t ** hook_list, const char *name, const char *command)
+{
+  wzd_hook_t * current_hook, * new_hook;
+
+  new_hook = malloc(sizeof(wzd_hook_t));
+  if (!new_hook) return 1;
+
+  new_hook->mask = EVENT_SITE;
+  new_hook->hook = NULL;
+  new_hook->opt = strdup(name);
+  new_hook->external_command = strdup(command);
+  new_hook->next_hook = NULL;
+
+  current_hook = *hook_list;
+
+  if (!current_hook) {
+    *hook_list = new_hook;
+    return 0;
+  }
+
+  while (current_hook->next_hook) {
+    current_hook = current_hook->next_hook;
+  }
+
+  current_hook->next_hook = new_hook;
+
+  return 0;
+
+}
+
+int hook_call_custom(wzd_context_t * context, wzd_hook_t *hook, const char *args)
+{
+  char buffer[1024];
+  FILE *command_output;
+  unsigned int l_command;
+
+  if (!hook || !hook->external_command) return 1;
+  l_command = strlen(hook->external_command);
+  if (l_command+strlen(args)>=1022) return 1;
+  strcpy(buffer,hook->external_command);
+  *(buffer+l_command++) = ' ';
+  strcpy(buffer+l_command,args);
+  if ( (command_output = popen(buffer,"r")) == NULL ) {
+    out_log(LEVEL_HIGH,"Hook '%s': unable to popen\n",hook->external_command);
+    return 1;
+  }
+  while (fgets(buffer,1023,command_output) != NULL)
+  {
+    send_message_raw(buffer,context);
+  }
+  pclose(command_output);
+
+  return 0;
+}
+
+int hook_call_external(wzd_hook_t *hook, const char *args)
+{
+  char buffer[1024];
+  FILE *command_output;
+  unsigned int l_command;
+
+  if (!hook || !hook->external_command) return 1;
+  l_command = strlen(hook->external_command);
+  if (l_command+strlen(args)>=1022) return 1;
+  strcpy(buffer,hook->external_command);
+  *(buffer+l_command++) = ' ';
+  strcpy(buffer+l_command,args);
+  if ( (command_output = popen(buffer,"r")) == NULL ) {
+    out_log(LEVEL_HIGH,"Hook '%s': unable to popen\n",hook->external_command);
+    return 1;
+  }
+  while (fgets(buffer,1023,command_output) != NULL)
+  {
+    out_log(LEVEL_INFO,"hook: %s\n",buffer);
+  }
+  pclose(command_output);
+
+  return 0;
+}
+
+unsigned long str2event(const char *s)
+{
+  int i=0;
+
+  while (event_tab[i].mask != 0)
+  {
+    if (strcasecmp(s,event_tab[i].name)==0) return event_tab[i].mask;
+    i++;
+  }
+  return 0;
+}
+
+
+
+/** check a module file */
 int module_check(const char *filename)
 {
   char path[1024];
@@ -179,7 +321,7 @@ int module_check(const char *filename)
   return 0;
 }
 
-/* add a module to the list */
+/** add a module to the list */
 int module_add(wzd_module_t ** module_list, const char *name)
 {
   wzd_module_t * current_module, * new_module;
@@ -206,7 +348,7 @@ int module_add(wzd_module_t ** module_list, const char *name)
   return 0;
 }
 
-/* load a module - module really should have been checked before ! */
+/** load a module - module really should have been checked before ! */
 int module_load(wzd_module_t *module)
 {
   char path[1024];
@@ -246,4 +388,28 @@ int module_load(wzd_module_t *module)
   ret = (f_init)();
 
   return ret;
+}
+
+/** free module list */
+void module_free(wzd_module_t ** module_list)
+{
+  wzd_module_t * current_module, * next_module;
+
+  current_module = *module_list;
+
+  while (current_module) {
+    next_module = current_module->next_module;
+
+    if (current_module->name)
+      free(current_module->name);
+#ifdef DEBUG
+    current_module->name = NULL;
+    current_module->next_module = NULL;
+#endif /* DEBUG */
+    free(current_module);
+
+    current_module = next_module;
+  }
+
+  *module_list = NULL;
 }
--- src/wzd_mod.h
+++ src/wzd_mod.h
@@ -1,12 +1,42 @@
+/*
+ * wzdftpd - a modular and cool ftp server
+ * Copyright (C) 2002-2003  Pierre Chifflier
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * As a special exemption, Pierre Chifflier
+ * and other respective copyright holders give permission to link this program
+ * with OpenSSL, and distribute the resulting executable, without including
+ * the source code for OpenSSL in the source distribution.
+ */
+
 #ifndef __WZD_MOD_H__
 #define __WZD_MOD_H__
 
 /* free hook list */
-int hook_free(wzd_hook_t **hook_list);
+void hook_free(wzd_hook_t **hook_list);
 
 /* register a new hook */
 int hook_add(wzd_hook_t ** hook_list, unsigned long mask, void_fct hook);
 int hook_add_external(wzd_hook_t ** hook_list, unsigned long mask, const char *command);
+int hook_add_custom_command(wzd_hook_t ** hook_list, const char *name, const char *command);
+
+int hook_call_custom(wzd_context_t * context, wzd_hook_t *hook, const char *args);
+int hook_call_external(wzd_hook_t *hook, const char *args);
+
+unsigned long str2event(const char *s);
 
 #define FORALL_HOOKS(test_mask)	{ \
   wzd_hook_t * hook; \
@@ -19,7 +49,10 @@ int hook_add_external(wzd_hook_t ** hook_list, unsigned long mask, const char *c
 }
 
 /* module hook struct, used in modules */
-typedef struct { unsigned long	event_id; void_fct	fct; } module_hook_t;
+typedef struct {
+  unsigned long event_id;
+  void_fct fct;
+} module_hook_t;
 
 /* check a module file */
 int module_check(const char *filename);
@@ -29,6 +62,9 @@ int module_add(wzd_module_t ** module_list, const char * name);
 
 /* load a module - module really should have been checked before ! */
 int module_load(wzd_module_t *module);
+
+/* free module list */
+void module_free(wzd_module_t ** module_list);
 
 /********************************/
 /* modules functions prototypes */
