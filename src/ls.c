6af81aedb44250f9482b9b2ace36af3a9fffbb79
Pierre Chifflier <chifflier@inl.fr>
2003-04-01 15:06:21 +0000

    Added license Added per user/group/flag vfs support

--- src/ls.c
+++ src/ls.c
@@ -1,3 +1,27 @@
+/*
+ * wzdftpd - a modular and cool ftp server
+ * Copyright (C) 2002-2003  Pierre Chifflier
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * As a special exemption, Pierre Chifflier
+ * and other respective copyright holders give permission to link this program
+ * with OpenSSL, and distribute the resulting executable, without including
+ * the source code for OpenSSL in the source distribution.
+ */
+
 
 /* ls replacement
    security reasons
@@ -102,6 +126,7 @@ int list(int sock,wzd_context_t * context,list_type_t format,char *directory,cha
 	    vfs = vfs->next_vfs;
 	    continue;
 	  }
+	if (!vfs_match_perm(vfs->target,user)) { vfs = vfs->next_vfs; continue; }
 	if (!list_match(ptr,mask)) { vfs = vfs->next_vfs; continue; }
         /* date */
         
