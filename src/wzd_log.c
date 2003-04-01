ef9356273b0b8a7e19d19d4c75bd4b9693f2a8b3
Pierre Chifflier <chifflier@inl.fr>
2003-04-01 14:54:56 +0000

    Added license Added str2loglevel function

--- src/wzd_log.c
+++ src/wzd_log.c
@@ -1,3 +1,27 @@
+/*
+ * wzdftpd - a modular and cool ftp server
+ * Copyright (C) 2002-2003  Pierre Chifflier
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * As a special exemption, Pierre Chifflier
+ * and other respective copyright holders give permission to link this program
+ * with OpenSSL, and distribute the resulting executable, without including
+ * the source code for OpenSSL in the source distribution.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
@@ -185,5 +209,16 @@ void log_message(const char *event, const char *fmt, ...)
       buffer
       );
   fflush(mainConfig->logfile);
+}
+
+int str2loglevel(const char *s)
+{
+  if (strcasecmp(s,"lowest")==0) return LEVEL_LOWEST;
+  else if (strcasecmp(s,"flood")==0) return LEVEL_FLOOD;
+  else if (strcasecmp(s,"info")==0) return LEVEL_INFO;
+  else if (strcasecmp(s,"normal")==0) return LEVEL_NORMAL;
+  else if (strcasecmp(s,"high")==0) return LEVEL_HIGH;
+  else if (strcasecmp(s,"critical")==0) return LEVEL_CRITICAL;
+  return -1;
 }
 
--- src/wzd_log.h
+++ src/wzd_log.h
@@ -1,3 +1,27 @@
+/*
+ * wzdftpd - a modular and cool ftp server
+ * Copyright (C) 2002-2003  Pierre Chifflier
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * As a special exemption, Pierre Chifflier
+ * and other respective copyright holders give permission to link this program
+ * with OpenSSL, and distribute the resulting executable, without including
+ * the source code for OpenSSL in the source distribution.
+ */
+
 #ifndef __WZD_LOG__
 #define __WZD_LOG__
 
@@ -27,5 +51,7 @@ void out_err(int level, const char *fmt,...);
 void out_xferlog(wzd_context_t * context, int is_complete);
 
 void log_message(const char *event, const char *fmt, ...);
+
+int str2loglevel(const char *s);
 
 #endif /* __WZD_LOG__ */
