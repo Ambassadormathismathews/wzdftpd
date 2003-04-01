0ffc1e3292883b32d8aa28782bb39c1ccc73d021
Pierre Chifflier <chifflier@inl.fr>
2003-04-01 15:02:06 +0000

    Added license Added cronjob_free function to exit properly

--- src/wzd_crontab.c
+++ src/wzd_crontab.c
@@ -1,3 +1,27 @@
+/*
+ * wzdftpd - a modular and cool ftp server
+ * Copyright (C) 2002-2003  Pierre Chifflier
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * As a special exemption, Pierre Chifflier
+ * and other respective copyright holders give permission to link this program
+ * with OpenSSL, and distribute the resulting executable, without including
+ * the source code for OpenSSL in the source distribution.
+ */
+
 #include <stdio.h>
 #include <time.h>
 #include <sys/types.h>
@@ -78,5 +102,28 @@ int cronjob_run(wzd_cronjob_t ** crontab)
   }
   
   return 0;
+}
+
+void cronjob_free(wzd_cronjob_t ** crontab)
+{
+  wzd_cronjob_t * current_job, * next_job;
+
+  current_job = *crontab;
+
+  while (current_job) {
+    next_job = current_job->next_cronjob;
+
+    if (current_job->command)
+     free(current_job->command);
+#ifdef DEBUG
+    current_job->interval = 0;
+    current_job->fn = NULL;
+    current_job->next_cronjob = NULL;
+#endif /* DEBUG */
+    free(current_job);
+
+    current_job = next_job;
+  }
+  *crontab = NULL;
 }
 
--- src/wzd_crontab.h
+++ src/wzd_crontab.h
@@ -1,3 +1,27 @@
+/*
+ * wzdftpd - a modular and cool ftp server
+ * Copyright (C) 2002-2003  Pierre Chifflier
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * As a special exemption, Pierre Chifflier
+ * and other respective copyright holders give permission to link this program
+ * with OpenSSL, and distribute the resulting executable, without including
+ * the source code for OpenSSL in the source distribution.
+ */
+
 #ifndef __WZD_CRONTAB__
 #define __WZD_CRONTAB__
 
@@ -5,6 +29,8 @@ struct wzd_cronjob_t;
 typedef struct wzd_cronjob_t wzd_cronjob_t;
 
 int cronjob_add(wzd_cronjob_t ** crontab, int (*fn)(void), const char * command, unsigned int interval);
+
+void cronjob_free(wzd_cronjob_t ** crontab);
 
 int cronjob_run(wzd_cronjob_t ** crontab);
 
