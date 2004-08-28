182f48e9f5cec1399f5a7d1120146dde7a898d77
Roman Bogorodskiy <novel@freebsd.org>
2004-08-28 00:30:26 +0000

    Fix build on FreeBSD 4.x (and probably other old BSDs) by changing includes order.

--- src/wzd_ServerThread.c
+++ src/wzd_ServerThread.c
@@ -28,6 +28,7 @@
 #include <winsock2.h>
 #else
 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -40,7 +41,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h> /* isspace */
-#include <sys/types.h>
 #include <sys/stat.h>
 #include <time.h>
 #include <errno.h>
