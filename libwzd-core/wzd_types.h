bb9f0e3b15928ab4d1b635d3fa9f250379d55b9b
Roman Bogorodskiy <novel@freebsd.org>
2005-07-16 18:59:47 +0000

    Fix IPv6 support on FreeBSD (and probably on OpenBSD). We used to undefine IN6_IS_ADDR_V4MAPPED but redefined it only on Windows.

--- libwzd-core/wzd_types.h
+++ libwzd-core/wzd_types.h
@@ -361,14 +361,14 @@ typedef signed fd_t;
 #define in6_addr in_addr6 /* funny ! */
 
 #define socklen_t	unsigned int
+#endif /* WINVER */
 
 #ifndef IN6_IS_ADDR_V4MAPPED
 #define IN6_IS_ADDR_V4MAPPED(a) \
 	((((const ULONG *)(a))[0] == 0) \
 	&& (((const ULONG *)(a))[1] == 0) \
 	&& (((const ULONG *)(a))[2] == htonl (0xffff)))
-#endif
-#endif /* WINVER */
+#endif /* IN6_IS_ADDR_V4MAPPED */
 
 #ifndef INADDR_NONE
 # define INADDR_NONE ((unsigned long int) 0xffffffff)
