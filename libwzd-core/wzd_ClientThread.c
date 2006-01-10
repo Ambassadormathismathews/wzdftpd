cea2e56bd5d6a82ba6e3a7acc3b4ec38bbbcc7f5
Pierre Chifflier <chifflier@inl.fr>
2006-01-10 14:13:10 +0000

    * bugfix: custom commands broken

--- libwzd-core/wzd_ClientThread.c
+++ libwzd-core/wzd_ClientThread.c
@@ -4058,54 +4058,69 @@ out_err(LEVEL_FLOOD,"<thread %ld> <- '%s'\n",(unsigned long)context->pid_child,s
       if (command->command)
         ret = (*(command->command))(token,command_buffer,context);
       else { /* external command */
+        char buffer_command[4096];
+        wzd_group_t * group;
+
+        if (user->group_num > 0) group = GetGroupByID(user->groups[0]);
+        cookie_parse_buffer(str_tochar(command->external_command), user, group, context, buffer_command, sizeof(buffer_command));
+        chop(buffer_command);
+        if (buffer_command[0] == '!') {
+          wzd_group_t * group;
+          if (user->group_num > 0) group = GetGroupByID(user->groups[0]);
+          do_site_print_file(buffer_command+1,user,group,context);
+        } else
 #ifndef WIN32
-        wzd_popen_t * p;
-        out_log(LEVEL_FLOOD,"DEBUG call external command [%s]\n",str_tochar(command->external_command));
-        p = my_popen(str_tochar(command->external_command));
-        if (p) {
-          FILE * file;
-          file = fdopen(p->fdr,"r");
-          /** \todo we don't know if the custom command will reply according
-           * to the RFC protocol, so we force the correct first and last lines
-           */
-          send_message_raw("200-\r\n",context);
-          while (fgets(buffer,sizeof(buffer)-1,file) != NULL)
-          {
-            send_message_raw(buffer,context);
+        {
+          wzd_popen_t * p;
+          out_log(LEVEL_FLOOD,"DEBUG call external command [%s]\n",buffer_command);
+          p = my_popen(buffer_command);
+          if (p) {
+            FILE * file;
+            file = fdopen(p->fdr,"r");
+            /** \todo we don't know if the custom command will reply according
+             * to the RFC protocol, so we force the correct first and last lines
+             */
+            send_message_raw("200-\r\n",context);
+            while (fgets(buffer,sizeof(buffer)-1,file) != NULL)
+            {
+              send_message_raw(buffer,context);
+            }
+            send_message_raw("200 Command OK\r\n",context);
+            fclose(file);
+            ret = my_pclose(p);
+          } else { /* !p */
+            out_log(LEVEL_NORMAL,"ERROR failed to popen command '%s'\n",buffer_command);
+            ret = send_message_with_args(501,context,"Command execution failed");
           }
-          send_message_raw("200 Command OK\r\n",context);
-          fclose(file);
-          ret = my_pclose(p);
-        } else { /* !p */
-          out_log(LEVEL_NORMAL,"ERROR failed to popen command '%s'\n",str_tochar(command->external_command));
-          ret = send_message_with_args(501,context,"Command execution failed");
         }
 #else /* WIN32 */
-		FILE * file;
-		char * clean_command;
-		extern void _cleanup_shell_command(char * buffer, size_t length);
-		clean_command = strdup(str_tochar(command->external_command));
-		_cleanup_shell_command(clean_command,strlen(clean_command));
+        {
+          FILE * file;
+          char * clean_command;
+          extern void _cleanup_shell_command(char * buffer, size_t length);
+          clean_command = strdup(buffer_command);
+          _cleanup_shell_command(clean_command,strlen(clean_command));
 
-		out_log(LEVEL_FLOOD,"DEBUG call external command [%s]\n",clean_command);
-		file = _popen(clean_command,"r");
-		if (file) {
-          /** \todo we don't know if the custom command will reply according
-           * to the RFC protocol, so we force the correct first and last lines
-           */
-          send_message_raw("200-\r\n",context);
-          while (fgets(buffer,sizeof(buffer)-1,file) != NULL)
-          {
-            send_message_raw(buffer,context);
+          out_log(LEVEL_FLOOD,"DEBUG call external command [%s]\n",clean_command);
+          file = _popen(clean_command,"r");
+          if (file) {
+            /** \todo we don't know if the custom command will reply according
+             * to the RFC protocol, so we force the correct first and last lines
+             */
+            send_message_raw("200-\r\n",context);
+            while (fgets(buffer,sizeof(buffer)-1,file) != NULL)
+            {
+              send_message_raw(buffer,context);
+            }
+            send_message_raw("200 Command OK\r\n",context);
+            _pclose(file);
+            ret = 0;
+          } else { /* !p */
+            out_log(LEVEL_NORMAL,"ERROR failed to popen command '%s'\n",clean_command);
+            ret = send_message_with_args(501,context,"Command execution failed");
           }
-          send_message_raw("200 Command OK\r\n",context);
-          _pclose(file);
-		  ret = 0;
-        } else { /* !p */
-          out_log(LEVEL_NORMAL,"ERROR failed to popen command '%s'\n",clean_command);
-          ret = send_message_with_args(501,context,"Command execution failed");
+          free(clean_command);
         }
-		free(clean_command);
 #endif /* WIN32 */
       }
       str_deallocate(token);
