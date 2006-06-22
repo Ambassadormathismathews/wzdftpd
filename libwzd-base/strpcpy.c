a1cf5928e89440cd9c394683bc95048ac7054359
Pierre Chifflier <chifflier@inl.fr>
2006-06-22 12:15:19 +0000

    Add new function strpcpy() for fast string concatenations

---
+++ libwzd-base/strpcpy.c
@@ -0,0 +1,45 @@
+/* vi:ai:et:ts=8 sw=2
+ */
+/*
+ * wzdftpd - a modular and cool ftp server
+ * Copyright (C) 2002-2004  Pierre Chifflier
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * As a special exemption, Pierre Chifflier
+ * and other respective copyright holders give permission to link this program
+ * with OpenSSL, and distribute the resulting executable, without including
+ * the source code for OpenSSL in the source distribution.
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "../config.h"
+#endif
+
+#include <string.h>
+
+/** Copy \a src, including the terminating null character
+ * to the destination specified by \a dst.
+ * No overflow checking is performed when strings are copied
+ *
+ * \return a pointer to the end (terminating null) of the copied string
+ */
+char * strpcpy(char * dst, const char * src)
+{
+  while ((*dst++ = *src++) != '\0')
+    /* copy source to destination */ ;
+  return (dst - 1);
+}
+
